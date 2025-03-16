10d5e6877b9e81fa9166789da058ec0b97f11875
MonsieurLoki <gaspard.derruine@gmail.com>
2025-03-16 15:03:19 +0100

    added around 100 movies from tmdb api in the DB

---
+++ backend/movies/management/commands/import_tmdb_movies.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from movies.tmdb import discover_movies
+from movies.models import Movie
+
+class Command(BaseCommand):
+    help = "Importer environ 100 films diversifiés depuis TMDB"
+
+    def handle(self, *args, **options):
+        total_imported = 0
+        pages_needed = 5  # 5 pages x 20 films par page = 100 films
+
+        for page in range(1, pages_needed + 1):
+            movies = discover_movies(page=page)
+            for movie_data in movies:
+                Movie.objects.update_or_create(
+                    title=movie_data['title'],
+                    defaults={
+                        'description': movie_data['overview'] or "Pas de description",
+                        'release_year': movie_data['release_date'][:4] if movie_data['release_date'] else 0,
+                        'genre': "Inconnu",
+                        'poster_url': f"https://image.tmdb.org/t/p/w500{movie_data['poster_path']}" if movie_data['poster_path'] else ""
+                    }
+                )
+                total_imported += 1
+
+        self.stdout.write(self.style.SUCCESS(f"{total_imported} films importés avec succès !"))
--- backend/movies/tmdb.py
+++ backend/movies/tmdb.py
@@ -1,16 +1,17 @@
 # movies/tmdb.py
 import requests
+from django.conf import settings
 
-API_KEY = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDY0N2IzZWE0MmJiMTZmYzViMTQ4NjYxODkzMTE1NCIsIm5iZiI6MTc0MjA1NzE4Ny45MjUsInN1YiI6IjY3ZDVhZWUzNTc3NjY1YWNlNWYxNzQ5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cflRKPnaInfrZsGErDd7iaQKTKPb_LwIrMBJWAKtcmM"
+api_key = settings.TMDB_API_KEY
 BASE_URL = "https://api.themoviedb.org/3"
 
 headers = {
     "accept": "application/json",
-    "Authorization": API_KEY
+    "Authorization": api_key
 }
 
-def get_popular_movies():
-    url = f"{BASE_URL}/movie/popular"
+def discover_movies(page=1):
+    url = f"{BASE_URL}/discover/movie?page={page}"
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()['results']
