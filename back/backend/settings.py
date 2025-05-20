f4df67460b6ee5af04824d18b8b74e7c9c79f7af
NoppeHugo <hugonoppe444@gmail.com>
2025-05-20 14:08:05 +0200

    Fix (smtp) : fonctionne en smtp maintenant

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -220,3 +220,12 @@ STATIC_URL = 'static/'
 
 # Clé par défaut des champs auto
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Envoi d’emails avec SMTP MailerSend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.mailersend.net")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "no-reply@critiq.ovh")
--- back/users/utils.py
+++ back/users/utils.py
@@ -2,33 +2,22 @@ import os
 import requests
 import random
 
+import random
+from django.core.mail import send_mail
+
 def generate_verification_code():
     return str(random.randint(100000, 999999))
 
 def send_verification_email(email, code):
-    MAILERSEND_API_KEY = os.environ.get("MAILERSEND_API_KEY")
-    if not MAILERSEND_API_KEY:
-        raise Exception("MAILERSEND_API_KEY not set")
-
-    data = {
-                "from": {
-            "email": "no-reply@critiq.ovh", 
-            "name": "Critiq"
-        },
-        "to": [{"email": email}],
-        "subject": "Code de vérification",
-        "text": f"Votre code de vérification est : {code}"
-    }
-
-    print(f"[DEBUG] Envoi à {email} avec code {code}")
-
-    response = requests.post(
-        "https://api.mailersend.com/v1/email",
-        json=data,
-        headers={"Authorization": f"Bearer {MAILERSEND_API_KEY}"}
-    )
-
-    if response.status_code >= 400:
-        raise Exception(f"Erreur d'envoi Mailersend: {response.text}")
+    subject = "Code de vérification"
+    message = f"Bonjour,\n\nVoici votre code de vérification : {code}\n\nL'équipe Critiq"
     
-
+    print(f"[DEBUG] Envoi SMTP à {email} avec code {code}")
+    
+    send_mail(
+        subject,
+        message,
+        None,  # Utilise DEFAULT_FROM_EMAIL
+        [email],
+        fail_silently=False
+    )
